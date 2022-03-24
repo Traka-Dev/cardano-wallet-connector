1a14f21b903ed1d7d10e091248e411d58606f11d
Traka Dev <trakadev@protonmail.com>
2022-03-24 10:34:04 -0600

    wasm integration and comments

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const wallets_compatible = [
     'yoroi',
     'eternl',
 ]
+/**
+ * 
+ * @param {window.cardano obj} cardano_window 
+ * @param {String} walletName 
+ * @returns {Wallet Object}
+ */
 export async function searchForWallet (cardano_window, walletName) {
     // Look if wallet is installed and compatible
     let found = wallets_compatible.find(e => e === walletName && cardano_window.hasOwnProperty(walletName))
@@ -15,7 +21,13 @@ export async function searchForWallet (cardano_window, walletName) {
         return wallet
     }else{ throw new Error('Compatible Wallet Not Found') }
 }
+/**
+ * 
+ * @param {cardano wallet enabled object} wallet 
+ * @param {String} blockfost_api 
+ * @param {Cardano Serialization Lib WASM} WASM 
+ */
+export async function wallet(wallet, blockfost_api, WASM=null){
+ if(WASM !== null) csl = WASM
 
-export async function wallet(wallet){
-    
 }
